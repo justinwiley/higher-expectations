4d5ee2da89d286963a641cdcfa0c4e34861adf35
Justin Wiley <justin.wiley@gmail.com>
2008-08-14 11:03:21 -0400

    Update project description

--- pkg/higher_expectations-0.0.1/spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'spec'
-require 'ruby-debug'
-
-require File.dirname(__FILE__) + '/../lib/higher_expectations'
-require File.dirname(__FILE__) + '/../lib/instance_methods'
